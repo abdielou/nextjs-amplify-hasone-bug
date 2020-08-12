dda56b9ef0d1a430c5dd8a8e018179ab589f875d
Abdiel Aviles <abdiel.aviles@gmail.com>
2020-08-12 10:05:22 -0400

    ADD: Create and Delete with DataStore

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 import Amplify from 'aws-amplify'
-import config from '../aws-exports';
+import config from '../aws-exports'
 import '../../styles/globals.css'
 
-Amplify.configure(config);
-Amplify.Logger.LOG_LEVEL = 'INFO';
+Amplify.configure(config)
+Amplify.Logger.LOG_LEVEL = 'INFO'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,93 @@
-import Head from 'next/head'
+import { useState, useEffect } from 'react'
+import { Auth, DataStore, Predicates } from 'aws-amplify'
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Project, Team } from '../models'
 import styles from '../../styles/Home.module.css'
 
 function Home() {
+  const [userData, setUserData] = useState()
+  const [projects, setProjects] = useState([])
+  const [teams, setTeams] = useState([])
+
+  useEffect(() => {
+    let dataSubs = []
+    //#region GQL Subscriptions
+    Auth.currentAuthenticatedUser()
+      .then(userData => {
+        setUserData(userData)
+        fetchData()
+        dataSubs.push(DataStore.observe(Project).subscribe(fetchData))
+        dataSubs.push(DataStore.observe(Team).subscribe(fetchData))
+      })
+
+    const unsubscribe = (subscription) => subscription && subscription.unsubscribe()
+    return () => {
+      console.log(`unsubscribing`, dataSubs.length)
+      dataSubs.forEach(unsubscribe)
+    }
+  }, [])
+
+  function fetchData() {
+    DataStore.query(Project, Predicates.ALL)
+      .then(projects => {
+        console.log('[DataStore]', 'projects', projects)
+        setProjects(projects);
+      })
+    DataStore.query(Team, Predicates.ALL)
+      .then(teams => {
+        console.log('[DataStore]', 'teams', teams)
+        setTeams(teams);
+      })
+  }
+
+  async function createProject() {
+    const { username: owner } = userData;
+    await DataStore.save(
+      new Team({
+        name: `Team-${Number(Date.now()).toString()}`
+       })
+    )
+      .then(async ({ id: teamID }) => {
+        await DataStore.save(
+          new Project({ teamID })
+        ).then(project => {
+          console.log(project)
+          fetchData()
+        });
+      })
+  }
+
+
+  function deleteProject(project) {
+    try {
+      console.warn(project)
+      DataStore.delete(Project, project.id);
+    } catch (error) {
+      console.error('[DataStore.deleteProject', error);
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <main className={styles.main}>
         <AmplifySignOut />
         <h1 className={styles.title}>
           Hello
         </h1>
+        <button onClick={() => createProject()}>CREATE</button>
+
+        {projects.map(project => {
+          const { id, name, teamID } = project
+          return (
+            <div key={id}>
+              <div>
+                {id} || {name} || {teamID}
+              </div>
+              <button onClick={() => deleteProject(project)}>DELETE</button>
+            </div>
+          )
+        })}
       </main>
 
     </div>
