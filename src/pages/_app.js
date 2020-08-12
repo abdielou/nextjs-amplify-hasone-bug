3087a082392cc2bc86402ab50307c43992ea7fc1
Abdiel Aviles <abdiel.aviles@gmail.com>
2020-08-11 21:36:19 -0400

    ADD: Authenticator

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import Amplify from 'aws-amplify'
+import config from '../aws-exports';
 import '../../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+Amplify.configure(config);
+Amplify.Logger.LOG_LEVEL = 'INFO';
+
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
